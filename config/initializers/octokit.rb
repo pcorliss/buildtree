653f2ca13c9c4bf2c01a8c9ee29a23fb821f5a85
kcboschert <kcboschert@gmail.com>
2015-08-25 15:05:13 -0500

    Add note to use Organization Permissions API Preview for older GHE instances. Enable auto-pagination for Octokit.

--- config/initializers/octokit.rb
+++ config/initializers/octokit.rb
@@ -1,6 +1,10 @@
-if host = ENV['GHE_HOST']
-  Octokit.configure do |c|
+Octokit.configure do |c|
+  if host = ENV['GHE_HOST']
     c.api_endpoint = "https://#{host}/api/v3"
     c.web_endpoint = "https://#{host}"
+    # Uncomment to use the Organization Permissions API Preview
+    # https://developer.github.com/enterprise/2.2/changes/2015-01-07-prepare-for-organization-permissions-changes/
+    # c.default_media_type = "application/vnd.github.moondragon+json"
   end
+  c.auto_paginate = true
 end
